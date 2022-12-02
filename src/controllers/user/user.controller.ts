b9dbea2b0221ed3ae1be6ea26b4348bc152b09d7
Ubuntu <ubuntu@ip-172-31-37-64.ap-south-1.compute.internal>
2022-12-02 23:39:44 +0530

    feat: verify kyc

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { createFailureResponse, createSuccessResponse } from '../../interfaces/response';
 import Controller from '../../interfaces/controller.interface';
 import CreateOrUpdateUserDto from './dto/createOrUpdateUser.dto';
+import VerifyUserDto from './dto/verifyUser.dto';
 import UserService from './user.service';
+import VerifyUserResponse from './interfaces/verifyUserResponse.interface';
 
 class UserController implements Controller {
   public router = express.Router();
@@ -15,6 +18,7 @@ class UserController implements Controller {
     this.router.get(`/users`, () => undefined);
     this.router.get(`/users/:walletAddress`, this.getUserByWalletAddress);
     this.router.post(`/users`, this.createOrUpdateUser);
+    this.router.post(`/users/verify`, this.verifyUser);
     this.router.put(`/users`, () => undefined);
     this.router.delete(`/users`, () => undefined);
   }
@@ -33,6 +37,24 @@ class UserController implements Controller {
   ) => {
     const { walletAddress } = request.params;
     response.send(await this.userService.getUserByWalletAddress({ walletAddress }));
+  };
+
+  private verifyUser = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const payload = request.body as VerifyUserDto;
+    let callbackFunction = (data : VerifyUserResponse) => {
+      console.log("-------")
+
+      console.log("-------")
+      if (data.success) {
+        return response.send(createSuccessResponse(data));
+      }
+      return response.send(createFailureResponse(404, `Failed verification for address ${payload.walletAddress}`));
+    }
+    this.userService.verifyUser({ payload }, callbackFunction);
+    // response.send(await this.userService.verifyUser({ payload }));
   };
 }
 
