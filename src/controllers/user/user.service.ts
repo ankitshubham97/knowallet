d2af7c7eae0f91eebdf2d98b3e8f07b7f5be6a72
Ubuntu <ubuntu@ip-172-31-37-64.ap-south-1.compute.internal>
2022-12-02 23:35:16 +0530

    fix: sc

--- src/controllers/user/user.service.ts
+++ src/controllers/user/user.service.ts
@@ -18,19 +18,6 @@ class UserService {
       const { walletAddress } = payload;
       console.log(walletAddress);
 
-      // // @ts-ignore
-      // const walletEntry = await this.userRepository.findOne({
-      //   where: {
-      //     walletAddress
-      //   }
-      // });
-      // if (!walletEntry) {
-      //   // @ts-ignore
-      //   await this.userRepository.create ({
-      //     walletAddress,
-      //   });
-      // }
-
       // Call mrz to extract age.
       const mrzScript = spawn('python3', ['python/mrz.py']);
       mrzScript.stdout.on('data', (data) => {
@@ -42,6 +29,8 @@ class UserService {
             return
           }
           console.log('faces verified:', result);
+          this.generateProofAndPersist({walletAddress});
+          this.generateCalldataAndPersist({walletAddress});
         });
         deepfaceScript.stderr.on('data', (data) => {
           console.log((data.toString()));
@@ -57,15 +46,102 @@ class UserService {
       mrzScript.on('exit', (code) => {
         console.log("Process quit with code : " + code);
       });
-
-      // Call deepface to verify faces.
-      
       
       return createSuccessResponse('OK');
     } catch (e) {
       logger.error(e);
       return createFailureResponse(500, INTERNAL_SERVER_ERROR);
     }
+  }
+  
+  public async getUserByWalletAddress({ walletAddress }: { walletAddress: string }) {
+    // @ts-ignore
+    const user = await this.userRepository.findOne({
+      where: {
+        walletAddress
+      }
+    });
+    if (!user) {
+      return createFailureResponse(400, `User with wallet address ${walletAddress} not found`);
+    }
+    return createSuccessResponse(user);
+  }
+
+
+  private generateProofAndPersist({walletAddress}: {walletAddress: string}) {
+    const genProofScript = spawn('bash', ['/home/ubuntu/workspace/api/zk-age-constraint/scripts/generate_proof.sh']);
+    genProofScript.stdout.on('data', (data) => {
+      const proof = String(data.toString()).trim();
+      console.log(proof, walletAddress);
+      // @ts-ignore
+      this.userRepository.findOne({
+        where: {
+          walletAddress
+        }
+      }).then( walletEntry => {
+        if (!walletEntry) {
+          // @ts-ignore
+          this.userRepository.create ({
+            walletAddress,
+            proof
+          });
+        } else {
+          // @ts-ignore
+          this.userRepository.update({
+            proof,
+          },
+          {
+            where: {
+              walletAddress
+            },
+          })
+        }
+      })
+    });
+    genProofScript.stderr.on('data', (data) => {
+      console.log((data.toString()));
+    });
+    genProofScript.on('exit', (code) => {
+      console.log("Process quit with code : " + code);
+    });
+  }
+
+  private generateCalldataAndPersist({walletAddress}: {walletAddress: string}) {
+    const genCalldataScript = spawn('bash', ['/home/ubuntu/workspace/api/zk-age-constraint/scripts/generate_calldata.sh']);
+    genCalldataScript.stdout.on('data', (data) => {
+      const calldata = String(data.toString()).trim();
+      console.log(calldata, walletAddress);
+      // @ts-ignore
+      this.userRepository.findOne({
+        where: {
+          walletAddress
+        }
+      }).then( walletEntry => {
+        if (!walletEntry) {
+          // @ts-ignore
+          this.userRepository.create ({
+            walletAddress,
+            calldata
+          });
+        } else {
+          // @ts-ignore
+          this.userRepository.update({
+            calldata,
+          },
+          {
+            where: {
+              walletAddress
+            },
+          })
+        }
+      })
+    });
+    genCalldataScript.stderr.on('data', (data) => {
+      console.log((data.toString()));
+    });
+    genCalldataScript.on('exit', (code) => {
+      console.log("Process quit with code : " + code);
+    });
   }
 }
 
