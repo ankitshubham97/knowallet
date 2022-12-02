68b12c271d82d848ceee4e2310c12b1dedad9bf1
Ankit Shubham <ankitshubham97@gmail.com>
2022-12-02 23:51:16 +0530

    fix: call ai endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,6 @@ export const SUCCESS = 'Success';
 export const NOT_FOUND = 'Not Found';
 export const INVALID_REQUEST = 'Invalid Request';
 export const INTERNAL_SERVER_ERROR = 'Internal Server Error';
+
+export const AI_URL = 'http://3.111.71.221:5000';
+export const LEGAL_AGE = 18;
