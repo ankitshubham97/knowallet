8760a709bb8e02f934947f77315656014c4fff0b
Ubuntu <ubuntu@ip-172-31-36-116.ap-south-1.compute.internal>
2022-12-03 01:38:16 +0530

    fix: update SC

--- zk-country-constraint/scripts/generate_proof.sh
+++ zk-country-constraint/scripts/generate_proof.sh
@@ -3,7 +3,6 @@
 cd /home/ubuntu/workspace/hawkeye/api/zk-age-constraint/circuit_js
 input_file=$(mktemp)
 echo $1 > $input_file
-echo $input_file > tmp.txt
 node generate_witness.js circuit.wasm $input_file ../witness.wtns
 cd ../
 snarkjs plonk prove circuit_final.zkey witness.wtns proof1.json public1.json
@@ -14,6 +13,5 @@ echo ", \"public\": " >> e.json
 cat public1.json >> e.json
 echo "}" >> e.json
 
-rm proof1.json public1.json
-# rm $input_file
+rm proof1.json public1.json $input_file
 cat e.json | jq -c .
